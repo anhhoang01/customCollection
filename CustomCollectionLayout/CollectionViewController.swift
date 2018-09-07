2c4b48a36882ea0684c2b7462af3278e377e6376
mac <anhhoang.gra@gmail.com>
2018-09-07 17:02:53 +0700

    design

--- CustomCollectionLayout/CollectionViewController.swift
+++ CustomCollectionLayout/CollectionViewController.swift
@@ -15,28 +15,41 @@
 import UIKit
 
 class CollectionViewController: UIViewController {
-
+    
     let contentCellIdentifier = "ContentCellIdentifier"
-
+    
     @IBOutlet weak var collectionView: UICollectionView!
     let arrHour = ["00:00","00:30","01:00","01:30","02:00","02:30","03:00","03:30","04:00","04:30","05:00","05:30","06:00","07:00","07:30","08:00","08:00","08:30","09:00","09:30","10:00","10:30","11:00","11:30","12:00","12:30","13:30","14:00","14:30","15:00","15:30","16:00","16:30","17:00","17:30","18:00","18:30","19:00","19:30","20:00","20:30","21:00","21:30","22:00","22:30","23:00","23:30"]
     let arrDate = ["1","2","3","4","5","6","7"]
+    
+    var arrData = [Data(time: "1 - 00:00", color: .red, data: "booked"),
+                   Data(time: "1 - 01:00", color: .red, data: "booked"),
+                   Data(time: "2 - 11:30", color: .red, data: "booked"),
+                   Data(time: "3 - 09:00", color: .red, data: "booked"),
+                   Data(time: "4 - 14:00", color: .red, data: "booked"),
+                   Data(time: "5 - 15:00", color: .red, data: "booked"),
+                   Data(time: "6 - 10:30", color: .red, data: "booked"),
+                   Data(time: "4 - 20:00", color: .red, data: "booked"),
+                   Data(time: "2 - 23:30", color: .red, data: "booked"),
+                   Data(time: "7 - 00:30", color: .red, data: "booked"),
+                   Data(time: "2 - 18:00", color: .red, data: "booked")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         collectionView.register(UINib(nibName: "ContentCollectionViewCell", bundle: nil),
                                 forCellWithReuseIdentifier: contentCellIdentifier)
     }
-
+    
 }
 
 // MARK: - UICollectionViewDataSource
 extension CollectionViewController: UICollectionViewDataSource {
-
+    
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return arrHour.count
+        return arrHour.count + 1
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return arrDate.count + 1
     }
@@ -45,61 +58,28 @@ extension CollectionViewController: UICollectionViewDataSource {
         // swiftlint:disable force_cast
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: contentCellIdentifier,
                                                       for: indexPath) as! ContentCollectionViewCell
-
         
-        if indexPath.section % 2 != 0 {
-            cell.backgroundColor = UIColor(white: 242/255.0, alpha: 1.0)
-        } else {
-            cell.backgroundColor = UIColor.white
-        }
-
-        if indexPath.section == 0 {
+        cell.backgroundColor = indexPath.section % 2 != 0 ? UIColor(white: 242/255.0, alpha: 1.0) : .white
+        
+        switch indexPath.section {
+        case 0:
+            cell.time = ""
+            cell.contentLabel.text = indexPath.row == 0 ? "" : arrDate[indexPath.row - 1]
+        default:
             if indexPath.row == 0 {
-                cell.contentLabel.text = ""
-            } else  {
-                for i in 1...7 {
-                    if indexPath.row == i {
-                        cell.contentLabel.text = arrDate[i-1]
-                    }
-                }
-            }
-        } else {
-            if indexPath.row == 0 {
+                cell.time = ""
                 cell.contentLabel.text = arrHour[indexPath.section - 1]
-            } else if indexPath.row == 1 {
-                for i in 1...indexPath.section{
-                    //cell.contentLabel.text = "\(i)"
-                }
-            } else if indexPath.row == 2 {
-                for i in 1...indexPath.section{
-                    //cell.contentLabel.text = "\(i)"
-                }
-            } else if indexPath.row == 3 {
-                for i in 1...indexPath.section{
-                    //cell.contentLabel.text = "\(i)"
-                }
-            } else if indexPath.row == 4 {
-                for i in 1...indexPath.section{
-                    //cell.contentLabel.text = "\(i)"
-                }
-            } else if indexPath.row == 5 {
-                for i in 1...indexPath.section{
-                    //cell.contentLabel.text = "\(i)"
-                }
-            } else if indexPath.row == 6 {
-                for i in 1...indexPath.section{
-                    //cell.contentLabel.text = "\(i)"
-                }
             } else {
-                for i in 1...indexPath.section{
-                    //cell.contentLabel.text = "\(i)"
-                }
+                let time = "\(arrDate[indexPath.row - 1]) - \(arrHour[indexPath.section - 1])"
+                cell.time = time
+                cell.contentLabel.text = arrData.filter({$0.time == time}).first?.data
+                cell.background.backgroundColor = arrData.filter({$0.time == time}).first?.color
             }
         }
-
+        
         return cell
     }
-
+    
 }
 
 // MARK: - UICollectionViewDelegate
@@ -107,5 +87,18 @@ extension CollectionViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         print(indexPath.row)
         print("->",indexPath.section)
+        
     }
 }
+
+class Data: NSObject {
+    var time = ""
+    var color : UIColor = UIColor.red
+    var data  = ""
+    init(time: String, color: UIColor, data:String) {
+        self.time = time
+        self.color = color
+        self.data = data
+    }
+}
+
--- CustomCollectionLayout/ContentCollectionViewCell.swift
+++ CustomCollectionLayout/ContentCollectionViewCell.swift
@@ -11,10 +11,29 @@ import UIKit
 class ContentCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet weak var contentLabel: UILabel!
-
+    var time = ""
+    @IBOutlet weak var background: UIView!
+    @IBOutlet weak var boderView: UIView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupUI()
+    }
+    
+    private func setupUI() {
+        self.layer.borderWidth = 1
+        self.layer.masksToBounds = false
+        self.clipsToBounds = true
+        self.layer.cornerRadius = 0
+        self.layer.borderColor = UIColor.darkGray.cgColor
     }
 
 }
